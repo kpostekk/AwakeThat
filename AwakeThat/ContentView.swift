cc9e7d5ffcd045c0dd80e61157708fa0e41da173
Krystian Postek <krystian@postek.eu>
2021-08-08 09:21:35 +0200

    Replace Loaf with AlertToast

--- AwakeThat/ContentView.swift
+++ AwakeThat/ContentView.swift
@@ -5,53 +5,61 @@
 //  Created by Krystian Postek on 07/08/2021.
 //
 
+import AlertToast
 import Awake
 import CoreData
-import Loaf
 import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-    
+
     @State private var showingSheet = false
+    @State private var showingToast = false
+
     @State private var editingDevice: DeviceWake?
 
     @FetchRequest(sortDescriptors: [
         NSSortDescriptor(keyPath: \DeviceWake.alias, ascending: true)
     ], animation: .default)
     private var devices: FetchedResults<DeviceWake>
-    
+
     var body: some View {
         NavigationView {
             List(devices) { device in
                 NavigationLink(destination: DeviceDetailsView(device: device)) {
                     DeviceRow(device: device)
-                    .contextMenu(ContextMenu(menuItems: {
-                        Button(action: { device.awakeDevice() }) {
-                            Label("Send magic packet", systemImage: "power")
-                        }
-                        Button(action: { showEditSheet(device) }) {
-                            Label("Edit", systemImage: "pencil")
-                        }
-                    }))
+                        .contextMenu(ContextMenu(menuItems: {
+                            Button(action: {
+                                showingToast.toggle()
+                                device.awakeDevice()
+                            }) {
+                                Label("Send magic packet", systemImage: "power")
+                            }
+                            Button(action: { showEditSheet(device) }) {
+                                Label("Edit", systemImage: "pencil")
+                            }
+                        }))
                 }
             }
             .navigationTitle("Devices")
             .navigationBarItems(trailing: Button(action: showSheet) {
                 Label("Add", systemImage: "plus")
             }
-            .sheet(isPresented: $showingSheet) {
-                FormSheet(editDevice: $editingDevice)
-            }
             )
         }
+        .sheet(isPresented: $showingSheet) {
+            FormSheet(editDevice: $editingDevice)
+        }
+        .toast(isPresenting: $showingToast) {
+            AlertToast(displayMode: .alert, type: .complete(.green), title: "Sent!")
+        }
     }
-    
+
     private func showEditSheet(_ device: DeviceWake) {
         editingDevice = device
         showingSheet.toggle()
     }
-    
+
     private func showSheet() {
         showingSheet.toggle()
     }
--- AwakeThat/WakingUp.swift
+++ AwakeThat/WakingUp.swift
@@ -6,16 +6,10 @@
 //
 
 import Awake
-import Loaf
-import UIKit
 
 extension DeviceWake {
     func awakeDevice(useLoaf: Bool = true) {
         let awDevice = Awake.Device(MAC: self.macAddr!, BroadcastAddr: self.brAddr!, Port: UInt16(self.port))
         _ = Awake.target(device: awDevice)
-        
-        if useLoaf {
-            Loaf("Magic packet sent!", state: .success, sender: UIApplication.shared.windows.filter {$0.isKeyWindow}.first!.rootViewController!).show()
-        }
     }
 }
